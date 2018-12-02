d6993874ee58bb9ca7f60e093bd48498c9d12c6b
Venreaver <reaversindustry@gmail.com>
2018-12-02 16:57:46 +0300

    maven: first puzzle is done - added second option for solving the issue

--- module2/src/test/java/io/qala/javatraining/FailingTest.java
+++ module2/src/test/java/io/qala/javatraining/FailingTest.java
@@ -3,14 +3,27 @@ package io.qala.javatraining;
 
 import org.testng.annotations.Test;
 
+import java.io.FileInputStream;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 import static org.junit.Assert.assertEquals;
 
 public class FailingTest {
+    // Solving by using InputStream instead of FileInputStream
     @Test
     public void strangeFailure() throws Exception {
         InputStream is = getClass().getResourceAsStream("/myresource.txt");
         assertEquals(is.read(), 49);
+    }
+
+    // Solving by using the temp copy of the file
+    @Test
+    public void strangeFailureAnotherOption() throws Exception {
+        Path temp = Files.createTempFile("myresource", ".txt");
+        Files.copy(getClass().getResourceAsStream("/myresource.txt"), temp, StandardCopyOption.REPLACE_EXISTING);
+        assertEquals(new FileInputStream(temp.toFile()).read(), 49);
     }
 }
