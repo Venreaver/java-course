c672ff27ed5f0649bbf026944c8cd0c43ebe917f
Venreaver <reaversindustry@gmail.com>
2018-12-02 16:38:31 +0300

    maven: first puzzle is done

--- module2/src/test/java/io/qala/javatraining/FailingTest.java
+++ module2/src/test/java/io/qala/javatraining/FailingTest.java
@@ -3,13 +3,14 @@ package io.qala.javatraining;
 
 import org.testng.annotations.Test;
 
-import java.io.FileInputStream;
+import java.io.InputStream;
 
 import static org.junit.Assert.assertEquals;
 
 public class FailingTest {
-    @Test public void strangeFailure() throws Exception {
-        String file = ClassLoader.getSystemResource("myresource.txt").getFile();
-        assertEquals(new FileInputStream(file).read(), 49);
+    @Test
+    public void strangeFailure() throws Exception {
+        InputStream is = getClass().getResourceAsStream("/myresource.txt");
+        assertEquals(is.read(), 49);
     }
 }
